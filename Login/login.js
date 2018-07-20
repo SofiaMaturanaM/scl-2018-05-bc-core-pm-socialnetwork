a17fe533081f13413123c0dc21ef9ce9c652406a
SofiaMaturanaM <sofimatsu@gmail.com>
2018-07-19 23:34:35 -0400

    login validation (hecho por Mia, github no funciona)

--- Login/login.js
+++ Login/login.js
@@ -78,3 +78,50 @@ function facebookLoginWithFirebase() {
             console.log("Error de firebase > Mensaje > " + error.message); //error.message nos mostrará el mensaje de firebase del mismo error
         });
 }
+
+document.getElementById("login").onclick = function(email, password) {
+    let userEmailInput = document.getElementById("userEmail").value;
+    let userPasswordInput = document.getElementById("userPassword").value;
+    if (validateEmailAndPassword(userEmailInput, userPasswordInput)) {
+        let userName = document.getElementById("user-name").value;
+        localStorage.setItem("user-name", userName);
+        document.getElementById("user-name").innerHTML = "Mia";
+        location = "timelineIndex.html";
+    } else {
+        alert("Invalid username or password")
+    }
+}
+
+function validateEmailAndPassword(email, password) {
+    if (defaultUserEmail === email && defaultUserPassword === password) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isValidEmailFormat(userInput) {
+    if (userInput.includes("@")) {
+        if (substr(userInput.indexOf("@")).includes(".")) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+function isValidPasswordFormat(userInput) {
+    if (userInput.lenght <= 8 && isInteger(userInput)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isInteger(userInput) {
+    let inputIsANumber = parseInt(userInput) != NaN;
+    let inputIsAnInteger = userInput % 1 === 0;
+    return inputIsANumber && inputIsAnInteger;
+}
